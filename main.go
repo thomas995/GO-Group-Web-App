7a455d30d658f22926a20871c7e0dcb02cd2b33d
Claire Finn <Clairefinn74@yahoo.ie>
2016-10-14 17:31:03 +0100

    Added dropdown list

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
-import "gopkg.in/macaron.v1"
+ import (
+         "net/http"
+ )
 
-func main() {
-    m := macaron.Classic()
-    m.Get("/", func() string {
-        return "My name is Claire!"
-    })
-    m.Run()
-}
+/* Dropdown list with Tradesperson's job titles
+ adapted from https://www.socketloop.com/tutorials/golang-populate-dropdown-with-html-template-example */
+ func SimpleSelectTag(w http.ResponseWriter, r *http.Request) {
+         html := `<!DOCTYPE html>
+ <html>
+ <body>
+ <select>
+   <option value="Beautician">Beautician</option>
+   <option value="Builder">Builder</option>
+   <option value="Carpenter">Carpenter</option>
+   <option value="Cleaner">Cleaner</option>
+   <option value="Delivery">Delivery</option>
+   <option value="Electrician">Electrician</option>
+   <option value="Farmer">Farmer</option>
+   <option value="Gardener">Gardener</option>
+   <option value="Hair and Beauty">Hair and Beauty</option>
+   <option value="Hairdresser">Hairdresser</option>
+   <option value="Mechanic">Mechanic</option>
+   <option value="Painter">Painter</option>
+   <option value="Plumber">Plumber</option>
+   <option value="Technician">Technician</option>
+   <option value="Tiler">Tiler</option>
+   <option value="Transport">Transport</option>
+   <option value="Other">Other</option>
+
+ </select>
+
+ </body>
+ </html>`
+
+         w.Write([]byte(html))
+ }
+
+ func main() {
+         mux := http.NewServeMux()
+         mux.HandleFunc("/", SimpleSelectTag)
+
+         http.ListenAndServe(":8080", mux)
+ }
