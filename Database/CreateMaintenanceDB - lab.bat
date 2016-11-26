1382a6351184ef9d15723280abf70cfaab1b8a7f
Alna132 <alanna@Joes-MacBook-Pro.local>
2016-11-26 16:00:45 +0000

    Updated the database.

---
+++ Database/CreateMaintenanceDB - lab.bat
@@ -0,0 +1,17 @@
+@ECHO off
+
+:: Batch file for
+:: Go Group Project at GMIT 2016
+:: Author: Alanna Curran
+:: Date:    Nov 17, 2016
+:: Adapted from: Murach's ADO.NET 4 with Visual Basic 2010
+:: 
+:: Uses the SQLCMD utility to run a SQL script that creates
+:: the Employee database.
+
+ECHO Attempting to create the Maintenance database . . . 
+sqlcmd -S localhost\SQLExpress2008 -E /i CreateEmployeeDB.sql
+ECHO.
+ECHO If no error message is shown, then the database was created successfully.
+ECHO.
+PAUSE
