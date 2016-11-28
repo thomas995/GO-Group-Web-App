cff76b6da1f86cd913a8cf83ebfbb1e1e44e0609
theultraduffmaster <g00318025@gmit.ie>
2016-11-28 20:18:20 +0000

    Commit Updated
    Fixed login so it serves up Internal html now and also updated look on all pages to conform to the angular bootstrap template

--- gowebapp/main.go
+++ gowebapp/main.go
@@ -1,299 +1,100 @@
+//https://dinosaurscode.xyz/go/2016/06/19/golang-mysql-authentication/
+
 package main
 
-// Adapted from: https://github.com/jakecoffman/go-angular-tutorial
-// Adapted from: https://mschoebel.info/2014/03/09/snippet-golang-webapp-login-logout/
-// Comments (Apart from the last one) are my own, info gathered from w3Schools and https://golang.org
-import (
-	//"flag"
-	"fmt"
-	//"log" // It defines a type, Logger, with methods for formatting output. 
-	"net/http"
-	"github.com/gorilla/mux" //need to type "go get github.com/gorilla/mux" into cmder to use (without the quotations obviously)
-	"github.com/gorilla/securecookie" //need to type "github.com/gorilla/securecookie" into cmder to use (without the quotations obviously)
-)
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
 
-// cookies are handled here
+import "golang.org/x/crypto/bcrypt"
 
-var cookieHandler = securecookie.New(
-	securecookie.GenerateRandomKey(64),
-	securecookie.GenerateRandomKey(32))
-//Reading cookies for username
-func getUserName(request *http.Request) (userName string) {
-	if cookie, err := request.Cookie("session"); err == nil {
-		cookieValue := make(map[string]string)
-		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
-			userName = cookieValue["email"]
+import "net/http"
+
+var db *sql.DB
+var err error
+
+func signupPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.ServeFile(res, req, "signup.html")
+		return
+	}
+
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+
+	var user string
+
+	err := db.QueryRow("SELECT username FROM users WHERE username=?", username).Scan(&user)
+
+	switch {
+	case err == sql.ErrNoRows:
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(res, "Server error, unable to create your account.", 500)
+			return
 		}
-	}
-	return userName
-}
 
-//Saves username in map then encodes with value map and stores that in a cookie
-func setSession(userName string, response http.ResponseWriter) {
-	value := map[string]string{
-		"email": userName,
-	}
-	if encoded, err := cookieHandler.Encode("session", value); err == nil {
-		cookie := &http.Cookie{
-			Name:  "session",
-			Value: encoded,
-			Path:  "/",
+		_, err = db.Exec("INSERT INTO users(username, password) VALUES(?, ?)", username, hashedPassword)
+		if err != nil {
+			http.Error(res, "Server error, unable to create your account.", 500)
+			return
 		}
-		http.SetCookie(response, cookie)
+
+		res.Write([]byte("User created!"))
+		return
+	case err != nil:
+		http.Error(res, "Server error, unable to create your account.", 500)
+		return
+	default:
+		http.Redirect(res, req, "/", 301)
 	}
 }
 
-//Returns to indexPage and clears cookies
-func clearSession(response http.ResponseWriter) {
-	cookie := &http.Cookie{
-		Name:   "session",
-		Value:  "",
-		Path:   "/",
-		MaxAge: -1,
+func loginPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.ServeFile(res, req, "login.html")
+		return
 	}
-	http.SetCookie(response, cookie)
-}
 
-// login handler handles the login for stored users
+	username := req.FormValue("username")
+	password := req.FormValue("password")
 
-func loginHandler(response http.ResponseWriter, request *http.Request) {
-	email := request.FormValue("email")
-	pass := request.FormValue("password")
-	redirectTarget := "/"
-	if email != "" && pass != "" {
-		// .. check credentials ..
-		setSession(email, response)
-		redirectTarget = "/internal"
+	var databaseUsername string
+	var databasePassword string
+
+	err := db.QueryRow("SELECT username, password FROM users WHERE username=?", username).Scan(&databaseUsername, &databasePassword)
+
+	if err != nil {
+		http.Redirect(res, req, "/login", 301)
+		return
 	}
-	http.Redirect(response, request, redirectTarget, 302)
-}
 
-
-// logout handler logs the current user out
-
-func logoutHandler(response http.ResponseWriter, request *http.Request) {
-	clearSession(response)
-	http.Redirect(response, request, "/", 302)
-}
-
-
-// index page contains code for login to application
-
-const indexPage = `
-<!-- Incorporating some HTML -->
-<head>
-<!-- Nav bar -->
-<form class="navbar-form navbar-left">
-	<title>WorkTracker</title>
-	</form>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<div class="container">
-		
-	</div>
-
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Work Tracker</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-
-        <form method="post" action="/login">
-    <input type="email" placeholder="Enter your email" id="email" name="email">
-    <input type="password" placeholder="Password" id="password" name="password">
-    <button type="submit" class="btn btn-success">Login</button>
-</form>    
-</div><!--/.navbar-collapse -->
-</div>
-</nav>
-`
-
-func indexPageHandler(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, indexPage)
-}
-
-// internal page contains code for the page user will see once successfully logged in
-
-const internalPage = `
-<h1>-</h1>
-<hr>
-<small>User: %s</small>
-<html ng-app> <!-- 'ng-app'' placed within a tag (in this case, the HTML tag)
-				allows HTML to become the route element for AngularJS.
-				All AngularJS applications must have a root element. Only one instance allowed. --> 
-<head>
-	<title>WorkTracker</title>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-<body>
-	<!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <!--Changed the top header here to include a name being entered and shown back to the user with a friendly hello-->
-      <div class="container">
-        <h2><input type="text" placeholder="Your Name here" ng-model="yes"></h2>
-		      <h3><p>Hello <span ng-bind="yes"></span></p></h3>
-        <p>This is a place where you can record all of your daily duties in one place, ready to show the boss. </p>
-      </div>
-    </div>
-
-	<div class="container">
-		
-	</div>
-
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-                     <form method="post" action="/logout">
-    <button type="submit" class="btn btn-success">Logout</button>
-          </button>
-          <a class="navbar-brand" href="#">Work Tracker</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right">
-  
-</form>
-          </form>
-        </div><!--/.navbar-collapse -->
-      </div>
-    </nav>
-
-	<div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Job Title</h2>
-          <!-- Dropdown list with Tradesperson's job titles
-  adapted from https://www.socketloop.com/tutorials/golang-populate-dropdown-with-html-template-example -->
-          <select>
-    <option value="Beautician">Beautician</option>
-    <option value="Builder">Builder</option>
-    <option value="Carpenter">Carpenter</option>
-    <option value="Cleaner">Cleaner</option>
-    <option value="Delivery">Delivery</option>
-    <option value="Electrician">Electrician</option>
-    <option value="Farmer">Farmer</option>
-    <option value="Gardener">Gardener</option>
-    <option value="Hair and Beauty">Hair and Beauty</option>
-    <option value="Hairdresser">Hairdresser</option>
-    <option value="Mechanic">Mechanic</option>
-    <option value="Painter">Painter</option>
-    <option value="Plumber">Plumber</option>
-    <option value="Technician">Technician</option>
-    <option value="Tiler">Tiler</option>
-    <option value="Transport">Transport</option>
-    <option value="Other">Other</option>
- <!-- Code adapted from http://stackoverflow.com/questions/30717105/adding-view-details-button-with-js -->
-  </select>
-          <details>
-    <summary>View Details</summary>
-    <p>
-        Please select your correct job title here from the dropdown list.
-    </p>
-</details>
-</div>
-<!-- Code adapted from http://www.w3schools.com/html/html_form_elements.asp -->
-        <div class="col-md-4">
-          <h2>Hours Worked This Week</h2>
-          <select name="HoursWorked">
-  <option value=">10 hours">>10 Hours</option>
-  <option value="10-15">10-15 Hours</option>
-  <option value="15-20">15-20 Hours</option>
-  <option value="25-30">25-30 Hours</option>
-  <option value="30-40">30-40 Hours</option>
-  <option value="40+">40+ Hours</option>
-
-</select>
-          <details>
-    <summary>View Details</summary>
-    <p>
-        Please select the correct amount of hours you worked this week.
-    </p>
-</details>
-       </div>
-       
-        <div class="col-md-4">
-          <h2>Additonal Information</h2>
-          <!--Adapted from http://www.w3schools.com/tags/tag_input.asp-->
-          <form action="demo_form.asp">
-              <input type="text" name="AddInfo" value=""><br>
-              <input type="submit" value="Submit">
-</form>
-<details>
-    <summary>View Details</summary>
-    <p>
-        Please add any additional information that you feel is relevent for your boss to know about regarding this week's work.
-    </p>
-</details>
-        </div>
-      </div>
-
-	  <hr>
-
-      <footer>
-        <p>&copy; 2016 WorkTracker, Inc.</p>
-      </footer>
-    </div> <!-- /container -->
-
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.3/angular.min.js"></script>
-</body>
-</html>
-
-`
-
-
-//Handles moving to internal page and bringing along userName
-func internalPageHandler(response http.ResponseWriter, request *http.Request) {
-	userName := getUserName(request)
-	if userName != "" {
-		fmt.Fprintf(response, internalPage, userName)
-	} else {
-		http.Redirect(response, request, "/", 302)
+	err = bcrypt.CompareHashAndPassword([]byte(databasePassword), []byte(password))
+	if err != nil {
+		http.Redirect(res, req, "/login", 301)
+		return
 	}
+
+	http.ServeFile(res, req, "Internal.html")
+	//res.Write([]byte("Hello" + databaseUsername))
 }
 
-// server main method which handles all the port hosting and http methods
-
-var router = mux.NewRouter()
-
+func homePage(res http.ResponseWriter, req *http.Request) {
+	http.ServeFile(res, req, "index.html")
+}
 func main() {
-	
-	
-	router.HandleFunc("/", indexPageHandler)
-	router.HandleFunc("/internal", internalPageHandler)
+	db, err = sql.Open("mysql", "b71da173aea4cf:05606ea1@tcp(eu-cdbr-azure-west-a.cloudapp.net:3306)/godatabase")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-	router.HandleFunc("/login", loginHandler).Methods("POST")
-	router.HandleFunc("/logout", logoutHandler).Methods("POST")
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
 
-//port := flag.Int("port", 8080, "port to serve on")
-//	dir := flag.String("directory", "web/", "directory of web files")
-	//flag.Parse()
-
-//	fs := http.Dir(*dir) // we have specified the root directory as the default: web
-//	fileHandler := http.FileServer(fs) //FileServer is a built-in to GO to allow serving of the html file(s)
-//	http.Handle("/", fileHandler) //Url routing, Gets file from path specified.
-
-	//log.Printf("Running on port %d\n", *port) //Logger accessible through helper function Print[f|ln]
-										    //That logger writes to standard error and prints the date and time of each logged message.
-	//addr := fmt.Sprintf("127.0.0.1:%d", *port)
-
-	http.Handle("/", router)
-	// this call blocks -- the progam runs here forever
-	//err := http.ListenAndServe(addr, nil)
+	http.HandleFunc("/signup", signupPage)
+	http.HandleFunc("/login", loginPage)
+	http.HandleFunc("/", homePage)
 	http.ListenAndServe(":8000", nil)
-	//fmt.Println(err.Error())
 }
